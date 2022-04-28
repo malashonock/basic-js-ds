1e35795fc9ef09eb36c1a6b70a17913d7c82d56b
Yan Malashonak <malashonock@gmail.com>
2022-04-28 09:25:51 +0300

    feat: implement stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #values;
+
+  constructor() {
+    this.#values = [];
+  }
+
+  get count() {
+    return this.#values.length;
+  }
+
+  push(value) {
+    this.#values.push(value);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.#values.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.#values.length > 0) {
+      return this.#values[this.count - 1];
+    } else {
+      return undefined;
+    }
   }
 }
 
+// const stack = new Stack();
+
+// stack.push(1); // adds the element to the stack
+// stack.peek(); // returns the peek, but doesn't delete it, returns 1
+// stack.pop(); // returns the top element from stack and deletes it, returns 1
+// stack.pop(); // undefined
+
 module.exports = {
-  Stack
+  Stack,
 };
